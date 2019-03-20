b0cba9fdcb4c655c6a27441b5c2b35c6ac8aa66b
Rodislav MOLDOVAN <rodislav@gmail.com>
2019-03-20 21:14:48 +0100

    added first controller, data object and exception

---
+++ src/main/java/tech/becoming/notificationservice/data/EmailRequest.java
@@ -0,0 +1,34 @@
+package tech.becoming.notificationservice.data;
+
+public class EmailRequest {
+
+    private String id;
+
+    private String from;
+
+    private String to;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+}
